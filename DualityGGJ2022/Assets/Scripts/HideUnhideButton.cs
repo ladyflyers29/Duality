d3c6d4d3093708cd3d4c0a2194afef2e9ffec63c
snberanek <36345444+snberanek@users.noreply.github.com>
2022-01-29 02:23:06 -0600

    puzzle technically works

--- DualityGGJ2022/Assets/Scripts/HideUnhideButton.cs
+++ DualityGGJ2022/Assets/Scripts/HideUnhideButton.cs
@@ -17,58 +17,21 @@ public class HideUnhideButton : MonoBehaviour
     }
 
     [SerializeField] public List<ThingToTurnOff> allThings = new List<ThingToTurnOff>();
-    GameObject item1;
-    GameObject item2;
+    public GameObject item1;
+    public GameObject item2;
 
     public int OneStay2Enter = 1; //1 = button 2 = pressure plate
 
     public AudioSource buttonSound;
 
-    public void OnTriggerStay(Collider col)
-    {
-        if (col.gameObject.tag == "Player" || col.gameObject.tag == "ball")
-        {
-            if (Input.GetMouseButtonDown(0)) //left click
-            {
-                if (OneStay2Enter == 1)
-                {
-                    if (item1.activeSelf == true)
-                    {
-                        item1.SetActive(false);
-                        item2.SetActive(true);
-                        buttonSound.Play();
-                    }
-
-                    else if (item1.activeSelf == false)
-                    {
-                        item1.SetActive(true);
-                        item2.SetActive(false);
-                        buttonSound.Play();
-                    }
-
-                }
-
-                
-
-            }
-          //  if (OneStay2Enter == 2 && col.gameObject.tag == "ball" && col.gameObject.tag != "Player")
-           // {
-           //     item1.SetActive(false);
-           //     item2.SetActive(true);
-         //   }
-
-            // Update other objects position and rotation
-
-            // thing.transform.rotation = Destination.transform.rotation;
-        }
-    }
+    
 
     
     public void OnTriggerEnter(Collider other)
     {
         if (OneStay2Enter != 2) return;
         Debug.Log(other);
-        if (other.gameObject.tag == "Player" || other.gameObject.tag == "ball")
+        if (other.gameObject.tag == "ball")
         {
             buttonSound.Play();
             foreach( ThingToTurnOff t in allThings ) {
@@ -84,7 +47,7 @@ public class HideUnhideButton : MonoBehaviour
     public void OnTriggerExit(Collider other)
     {
         if (OneStay2Enter != 2) return;
-        if (other.gameObject.tag == "Player" || other.gameObject.tag == "ball" )
+        if (other.gameObject.tag == "ball" )
         {
             foreach( ThingToTurnOff t in allThings ) {
                 if (t.gameObject != null) {
