7fdd1e76c57e9facaf65f4ed1b315b4fa72ce406
Detrian Hallem <detrian.hallem@trojans.dsu.edu>
2022-01-28 22:53:12 -0600

    Add switching between postproc profiles

--- DualityGGJ2022/Assets/Scripts/ChangeDaWorld.cs
+++ DualityGGJ2022/Assets/Scripts/ChangeDaWorld.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeDaWorld : MonoBehaviour
 {
+    const string LIGHTWORLDONLY = "LightWorldOnly";
+    const string DARKWORLDONLY = "DarkWorldOnly";
+    public static ChangeDaWorld daWorld { get; private set; } // Global DaWorld access, from any script
+    public static bool isDarkWorld { get; private set; }
+    public static event Action OnWorldSwitch; // Global thingy that other scripts can listen for
+
     public GameObject brightWorld; //parent object that holds bright world objects
     public GameObject darkWorld; //parent object that hold dark world objects
 
@@ -12,12 +19,37 @@ public class ChangeDaWorld : MonoBehaviour
 
     public AudioSource brightMusic;
     public AudioSource darkMusic;
+    GameObject[] lightWorldOnly = new GameObject[0];
+    GameObject[] darkWorldOnly = new GameObject[0];
 
-    // Update is called once per frame
+    void Awake() {
+        lightWorldOnly = GameObject.FindGameObjectsWithTag(LIGHTWORLDONLY);
+        darkWorldOnly = GameObject.FindGameObjectsWithTag(DARKWORLDONLY);
+        
+        if (daWorld != null) throw new System.Exception("More than one "+nameof(ChangeDaWorld)+" OH NOES");
+        daWorld = this;
+        ChangeDaWorld.OnWorldSwitch += DoWhenWorldSwitches; // this adds a listener to the world switch event
+        DoWhenWorldSwitches(); // run this right away to hide darkworld tagged stuff
+    }
+
+    // Turn off all gameobjects with 'LightWorldOnly' tag if 'isDarkWorld' is set to true, and vice-versa
+    void DoWhenWorldSwitches() {
+        foreach( GameObject g in lightWorldOnly ) {
+            g.SetActive(!isDarkWorld);
+        }
+        foreach( GameObject g in darkWorldOnly ) {
+            g.SetActive(isDarkWorld);
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E)) //if "E" is pressed
         {
+            isDarkWorld = !isDarkWorld;
+            ChangeDaWorld.OnWorldSwitch.Invoke(); // this calls the world switch event
+
+            if (brightWorld == null || darkWorld == null) return; // early-exit here if there is no bright or dark world
             if (brightWorld.activeSelf == true) //switching from bright world to dark world
             {
                 brightWorld.SetActive(false);
